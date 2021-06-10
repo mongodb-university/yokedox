ccaac6d2aec3e8e29360f64e741e751a57dcf402
Chris Bush <chris.bush@mongodb.com>
2021-06-10 17:51:10 -0400

    Update tests and makeProject

---
+++ cli/src/test/makeJsonFs.ts
@@ -0,0 +1,17 @@
+import { createFsFromVolume, DirectoryJSON, Volume } from "memfs";
+import { promises } from "fs";
+
+/**
+  Convenience function to make an fs.promises-compatible fs from a JSON
+  directory.
+
+  The keys of the provided object are virtual file paths and the values are the
+  string contents of those files. For example:
+
+      { "/path/to/foo.txt": "this is the content of foo.txt" }
+
+ */
+export function makeJsonFs(directoryJson: DirectoryJSON): typeof promises {
+  const fsFromVolume = createFsFromVolume(Volume.fromJSON(directoryJson));
+  return fsFromVolume.promises as unknown as typeof promises;
+}
