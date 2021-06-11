57d631cfae98e2f0e42d078e98a3eb12877533ff
Chris Bush <chris.bush@mongodb.com>
2021-06-11 15:38:50 -0400

    Work around Jest + ESM import issue
    - Transpile index.js in unist-util-visit as it is a pure ESM
    - Probably not required but couldn't figure out a cleaner way to do it
    - See https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c#im-having-problems-with-esm-and-jest

--- cli/jest.config.js
+++ cli/jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
   testRegex: "\\.test\\.ts$",
   modulePathIgnorePatterns: ["<rootDir>/build"],
+  // unist-util libraries like ESM - add exclusions to ignore patterns here
+  transformIgnorePatterns: ["node_modules/(?!unist-)"],
+  transform: {
+    "\\.(ts|tsx)$": "ts-jest",
+    "\\.(js|jsx)$": "babel-jest",
+  },
 };
