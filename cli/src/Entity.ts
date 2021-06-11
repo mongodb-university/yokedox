fdbcf2056b46b86ac3299ae40fc8681bb31a2883
Chris Bush <chris.bush@mongodb.com>
2021-06-11 10:49:35 -0400

    Simplify

--- cli/src/Entity.ts
+++ cli/src/Entity.ts
@@ -2,6 +2,10 @@
   Any uniquely-identifiable, documentable object.
  */
 export type Entity = {
-  id: string;
+  /**
+    Every entity has exactly one unique URI. This might include the page path
+    and subsection anchor (#fragment). The URI is the Entity's identity.
+   */
+  uri: string;
   // TODO
 };
