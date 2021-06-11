9effc7dcc326341d9f4515ff23383a082a7b787a
Chris Bush <chris.bush@mongodb.com>
2021-06-11 10:49:35 -0400

    Simplify

--- cli/src/Project.ts
+++ cli/src/Project.ts
@@ -1,56 +1,28 @@
-import { Entity } from "./Entity";
-import { LinkNode } from "./mdast";
 import { Page } from "./Page";
 
 /**
-  Represents a collection of documentated entities and pages to be written to
-  the filesystem.
+  Represents a collection of documentated pages to be written to the filesystem.
  */
 export type Project = {
   /**
-    To be called when an entity is discovered.
-
-    This resolves any pending links, which allows pages with pending links to be
-    written to disk.
-   */
-  addEntity(entity: Entity): void;
-
-  /**
-    Returns the entity with the given id if it has been added.
-   */
-  getEntity(id: string): Entity | undefined;
-
-  /**
-    Creates a link node for the given entity if it has been registered. If the
-    entity does not exist, issues a warning and creates a broken link node.
-   */
-  makeLink(args: MakeLinkArgs): LinkNode;
-
-  /**
     To be called when a page is complete and ready to be committed to the
     output.
+
+    Before writing to disk, we validate any links to other pages. A link is
+    "resolvable" if the target page was already passed to `writePage()`. If the
+    link has a fragment in the URI (i.e. a link to an 'anchor' or subsection
+    within a page), the link is only resolvable if that specific anchor was
+    added on that page.
+
+    If all links on the page are resolvable, the page gets written to disk.
+    Otherwise, the page is held until you call `finalize()`, at which point we
+    make a final attempt to resolve the links.
    */
   writePage(page: Page): void;
 
   /**
-    Flushes any remaining page writes.
+    Flushes any remaining page writes after a final attempt to resolve any links
+    to other entities.
    */
   finalize(): Promise<void>;
-};
-
-export type MakeLinkArgs = {
-  /**
-    The page uri the link originates from. This is used for error tracking. 
-   */
-  fromUri: string;
-
-  /**
-    The id of the entity to link to.
-   */
-  toEntityId: string;
-
-  /**
-    The text of the link. If undefined, uses the name or id of the target entity.
-   */
-  title?: string;
 };
