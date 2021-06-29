8e8e7b9f4b3efe0432cf7c1c915147ebb981e32a
Chris Bush <chris.bush@mongodb.com>
2021-06-28 21:48:43 -0400

    Use the more unified style

---
+++ cli/src/plugins/javadoc/scoopPhrasingNodesIntoParagraph.ts
@@ -0,0 +1,47 @@
+import { phrasing as isPhrasing } from "mdast-util-phrasing";
+import * as md from "mdast-builder";
+import { Node, Parent } from "../../mdast.js";
+import { Plugin, Transformer } from "unified";
+
+/**
+  All unwrapped 'phrasing' nodes should be in a paragraph, so scoop them into
+  paragraphs. See mdast's content model for details on 'phrasing'.
+ */
+export const scoopPhrasingNodesIntoParagraph: Plugin = (): Transformer => {
+  return (node: Node): Node => {
+    if (node.children === undefined) {
+      return node;
+    }
+    const root = node as Parent;
+    const children: Node[] = [];
+    let paragraph: Parent | undefined;
+    root.children.forEach((node) => {
+      if (node.type === "paragraph") {
+        // Start scooping subsequent non-phrasing nodes into this paragraph
+        paragraph = node as Parent;
+        children.push(paragraph);
+        return;
+      }
+      if (!isPhrasing(node)) {
+        // Non-phrasing nodes go after the current paragraph.
+        paragraph = undefined;
+        children.push(node);
+        return;
+      }
+      if (paragraph === undefined) {
+        // All phrasing nodes will be in a paragraph
+        paragraph = md.paragraph();
+        children.push(paragraph);
+      } else {
+        // Ensure separation between elements in the paragraph. TODO: Allow some
+        // punctuation to "hug" the previous element without the space.
+        paragraph.children.push(md.text(" "));
+      }
+      paragraph.children.push(node);
+    });
+    return {
+      ...root,
+      children,
+    };
+  };
+};
