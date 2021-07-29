6a46333ac4c76eeec6a9a6fc845479e3037690d5
Chris Bush <chris.bush@mongodb.com>
2021-07-29 14:23:04 -0400

    WIP

--- cli/src/mdastToRst.ts
+++ cli/src/mdastToRst.ts
@@ -1,98 +1,203 @@
-import { visit } from "unist-util-visit";
-import { MdastNodeType, Parent, RootNode, TypedNode } from "./mdast.js";
+import { strict as assert } from "assert";
+import { CONTINUE, SKIP, visit } from "unist-util-visit";
+import { MdastNodeType, Node, Parent, RootNode, TypedNode } from "./mdast.js";
 
 export type ToRstOptions = {
   //
 };
 
+/**
+  Converts the given mdast tree to an rST string.
+ */
 export const toRst = (root: RootNode, options?: ToRstOptions): string => {
-  const result: string[] = [];
-  const context: RstContext = {
-    indent: 0,
-  };
-  visit(root, undefined, (node, i, parent) => {
-    const handler = handlers[node.type as MdastNodeType];
-    if (handler === undefined) {
-      // handle undefined
+  const context = new RstContext(options);
+  context.addNode(root);
+  return context.rstString();
+};
+
+type Rst = { indent: number; lines: string[] };
+
+/**
+  Return the given string modified for escaping.
+ */
+type Escaper = (text: string) => string;
+
+class RstContext {
+  private indentationLevel = 0;
+  private rst: Rst[] = [];
+  private escapers: Escaper[] = [];
+
+  constructor(public options?: ToRstOptions) {}
+
+  rstString(): string {
+    return this.rst
+      .map(({ indent, lines }) => {
+        const indentString = new Array(indent).fill(" ").join();
+        return lines.join().replace(/\n/g, `\n${indentString}`);
+      })
+      .join();
+  }
+
+  indent(): void {
+    this.indentationLevel += 3;
+  }
+
+  indented(nodesOrText: Node[] | string): void {
+    this.indent();
+    if (typeof nodesOrText === "string") {
+      this.addText(nodesOrText);
     } else {
-      result.push(handler(context, node, i, parent));
+      this.addNodes(nodesOrText);
     }
-  });
-  return result.join();
-};
+    this.deindent();
+  }
 
-type RstContext = {
-  indent: number;
-};
+  deindent(): void {
+    this.indentationLevel -= 3;
+    assert(this.indentationLevel >= 0, "over-deindented!");
+  }
 
-type Handler<Type extends MdastNodeType> = (
+  addNewline(): void {
+    this.addText("\n");
+  }
+
+  addDoubleNewline(): void {
+    this.addText("\n\n");
+  }
+
+  addText(...text: string[]): void {
+    this.rst.push({ indent: this.indentationLevel, lines: text });
+  }
+
+  addNodes(nodes: Node[], escaper?: Escaper): void {
+    nodes.forEach((node) => this.addNode(node, escaper));
+  }
+
+  addNode(node: Node, escaper?: Escaper): void {
+    if (escaper) {
+      this.escapers.push(escaper);
+    }
+    const lastEscaperLength = this.escapers.length;
+    visit(node, undefined, (node, i, parent) => {
+      const type = node.type as MdastNodeType;
+      // Handler type is defined by node.type value
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const visitor = visitors[type] as Visitor<any> | undefined;
+      if (visitor === undefined) {
+        console.warn(`No handler for node type ${type}`);
+        return CONTINUE;
+      }
+      visitor(this, node, i, parent);
+      // Visitors are responsible for visiting node children
+      return SKIP;
+    });
+    if (escaper) {
+      assert(
+        lastEscaperLength === this.escapers.length,
+        "visitor did not remove added escapers!"
+      );
+      this.escapers.pop();
+    }
+  }
+}
+
+// https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#sections
+const titleAdornmentCharacters = ["^", "=", "-", "~", "_", "`"];
+
+type Visitor<Type extends MdastNodeType> = (
   context: RstContext,
   node: TypedNode<Type>,
   index: number | null,
   parent: Parent | null
-) => string;
+) => void;
 
-const handlers: {
-  [type in MdastNodeType]: Handler<type>;
+const visitors: {
+  [Type in MdastNodeType]: Visitor<Type>;
 } = {
-  blockquote(c, n, i, p) {
-    // TODO
-    return "";
+  blockquote(c, node) {
+    // https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#block-quotes
+    c.addDoubleNewline();
+    c.indented(node.children);
   },
-  break(c, n, i, p) {
-    // TODO
-    return "";
+  break(context) {
+    // https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#line-blocks
+    context.addNewline();
+    context.addText("| ");
   },
-  code(c, n, i, p) {
-    // TODO
-    return "";
+  code(c, { lang, value }) {
+    if (value === undefined) {
+      return;
+    }
+
+    c.addDoubleNewline();
+    c.addText(`.. code-block:: ${lang}\n\n`);
+    c.indented(value);
   },
-  emphasis(c, n, i, p) {
-    // TODO
-    return "";
+  emphasis(c, { children }) {
+    c.addText("*");
+    c.addNodes(children, (text) => text.replace(/\*/g, "\\*"));
+    c.addText("*");
   },
-  heading(c, n, i, p) {
-    // TODO
-    return "";
+  heading(c, { children, depth }) {
+    c.addDoubleNewline();
+    c.addNodes(children);
+    assert(1 <= depth && depth <= 6, `invalid heading depth: ${depth}`);
+    c.addText(new Array(4).fill(titleAdornmentCharacters[depth - 1]).join());
   },
   html(c, n, i, p) {
     // TODO
-    return "";
   },
   image(c, n, i, p) {
     // TODO
-    return "";
   },
-  inlineCode(c, n, i, p) {
-    // TODO
-    return "";
+  inlineCode(c, { value }) {
+    if (value === undefined) {
+      return;
+    }
+    c.addText(`\`\`${value}\`\``);
   },
   link(c, n, i, p) {
     // TODO
-    return "";
   },
   list(c, n, i, p) {
     // TODO
-    return "";
   },
   listItem(c, n, i, p) {
     // TODO
-    return "";
   },
-  paragraph(c, n, i, p) {
-    // TODO
-    return "";
+  paragraph(c) {
+    c.addDoubleNewline();
   },
-  root(c, n, i, p) {
-    // TODO
-    return "";
+  root(c, { children }) {
+    c.addNodes(children);
   },
-  strong(c, n, i, p) {
-    // TODO
-    return "";
+  separator(c) {
+    // https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#transitions
+    c.addDoubleNewline();
+    c.addText("----");
+    c.addDoubleNewline();
   },
-  text(c, n, i, p) {
+  strike() {
     // TODO
-    return "";
+  },
+  strong(c, { children }) {
+    c.addText("**");
+    c.addNodes(children, (text) => text.replace(/\*/g, "\\*"));
+    c.addText("**");
+  },
+  table() {
+    // TODO,
+  },
+  tableCell() {
+    // TODO,
+  },
+  tableRow() {
+    // TODO,
+  },
+  text(c, { value }) {
+    if (value === undefined) {
+      return;
+    }
+    c.addText(value);
   },
 };
